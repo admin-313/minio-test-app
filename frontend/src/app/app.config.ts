755d674a33079057ec5e9b769633af75ef1bba9b
progr <none@mail.com>
2024-12-19 18:11:32 +0200

    Dope comment

--- frontend/src/app/app.config.ts
+++ frontend/src/app/app.config.ts
@@ -3,6 +3,7 @@ import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 
+// The method of HTTP calls was discovered here: https://angular.dev/guide/http/setup
 export const appConfig: ApplicationConfig = {
   providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
 };
