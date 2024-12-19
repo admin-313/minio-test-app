e6998bdccaf1898b97610bef419e386ac7791096
progr <none@mail.com>
2024-12-19 15:08:54 +0200

    add 2 new components + routing for them

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { ImageGetterComponent } from './image-getter/image-getter.component';
+import { ImageViewerComponent } from './image-viewer/image-viewer.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: 'getimage', component: ImageGetterComponent },
+    { path: 'viewimage', component: ImageViewerComponent },
+];
